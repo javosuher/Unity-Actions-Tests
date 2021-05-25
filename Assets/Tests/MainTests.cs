4348698959ef6ca64152a5a5908ca7e54328743f
Javier Osuna Herrera <javier.osuna@nextlimit.com>
2021-05-25 13:56:47 +0200

    Added unit tests

---
+++ Assets/Tests/MainTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class MainTests
+    {
+        [Test]
+        public void MainTestsSimplePasses()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+    }
+}
